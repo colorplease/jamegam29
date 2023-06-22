3c8b8e42920507bcc7d0553a88aed9321d35d9d6
Brandon Wade <Brandon.d.wade42@gmail.com>
2023-06-22 15:58:51 -0700

    Updated LevelModule

--- jamegam29/Assets/LevelModule/Scripts/CameraHandler.cs
+++ jamegam29/Assets/LevelModule/Scripts/CameraHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using Cinemachine;
 using GameEvents;
 using UnityEngine;
@@ -10,9 +11,22 @@ namespace LevelModule.Scripts
         [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
         // Start is called before the first frame update
-        private void Start()
+        public Transform target; // Assign this to the player in the inspector
+        public float smoothSpeed = 0.125f; // Smoothing factor
+        public Vector3 offset; // Offset from the target (player)
+        public float margin = 0.2f; // Margin inside which player can move freely, adjust to your needs
+
+        void Update()
         {
-            initializeCameraEvent.RegisterListener(this);
+            Vector3 viewportPoint = Camera.main.WorldToViewportPoint(target.position);
+
+            // Check if the player is outside the viewport
+            if (viewportPoint.x < margin || viewportPoint.x > 1 - margin || viewportPoint.y < margin || viewportPoint.y > 1 - margin)
+            {
+                Vector3 desiredPosition = target.position + offset;
+                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.fixedDeltaTime);
+                transform.position = smoothedPosition;
+            }
         }
 
         private void OnDestroy()
@@ -27,8 +41,8 @@ namespace LevelModule.Scripts
             if (player == null)
                 return;
 
-            cinemachineVirtualCamera.m_LookAt = player.transform;
-            cinemachineVirtualCamera.Follow = player.transform;
+            //cinemachineVirtualCamera.m_LookAt = player.transform;
+           // cinemachineVirtualCamera.Follow = player.transform;
         }
     }
 }
