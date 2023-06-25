06942e0c0ee55ae7a965291be5f9e6affba93376
Brandon Wade <Brandon.d.wade42@gmail.com>
2023-06-25 14:41:43 -0700

    Implemented primitive main menu

---
+++ jamegam29/Assets/MainMenuHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        LoadSettings();
+
+        // Assuming the methods are in the same script as the sliders
+        musicSlider.onValueChanged.AddListener(delegate { SaveSettings(musicSlider.value, sfxSlider.value); });
+        sfxSlider.onValueChanged.AddListener(delegate { SaveSettings(musicSlider.value, sfxSlider.value); });
+    }
+
+    public void ButtonEvt_Exit()
+    {
+        Application.Quit();
+    }
+    
+    private void SaveSettings(float musicVolume, float sfxVolume)
+    {
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        PlayerPrefs.Save(); 
+    }
+
+    private void LoadSettings()
+    {
+        // PlayerPrefs.GetFloat returns 0 if "MusicVolume" or "SFXVolume" doesn't exist
+        float musicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f); // 1f is default value
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 1f); // 1f is default value
+
+        // Apply the loaded values to your sliders or audio mixer here. 
+        // Assuming you have references to your sliders or audio mixer.
+        musicSlider.value = musicVolume;
+        sfxSlider.value = sfxVolume;
+    }
+}
